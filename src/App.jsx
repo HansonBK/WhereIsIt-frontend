2fecf852cd714190f4cae14cbbb9d0d987d546d9
HansonBK <hansonhans88@gmail.com>
2026-07-17 12:31:39 -0400

    init the frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-emerald-400">WhereIsIt Frontend is Live!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
